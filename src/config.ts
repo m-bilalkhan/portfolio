b1b95d311639f4cddae2a1054fd956d7916530e6
m-bilalkhan <bilalkhan192003@gmail.com>
2026-01-20 15:57:33 +0500

    Updated - bug fix

--- src/config.ts
+++ src/config.ts
@@ -21,7 +21,7 @@ export const siteConfig = {
       ],
       link: "https://github.com/m-bilalkhan/event-announcement-system",
       skills: ["Terraform", "AWS", "Python", "REST APIs"],
-      image: "/public/event-announcement-platform.webp",
+      image: "/event-announcement-platform.webp",
     },
     {
       name: "Cloud Fun Facts Generator – Serverless, API-Driven, AI Integration Project", 
@@ -32,7 +32,7 @@ export const siteConfig = {
       ],
       link: "https://github.com/m-bilalkhan/cloud-fun-facts-generator",
       skills: ["Terraform", "AWS", "Python", "GitHub CI/CD"],
-      image: "/public/cloud-fun-facts-generator.gif",
+      image: "/cloud-fun-facts-generator.gif",
     },
     {
       name: "Visitor Log System – Personal Cloud Project",
@@ -46,7 +46,7 @@ export const siteConfig = {
       ],
       link: "https://github.com/m-bilalkhan/visitor-log-system/tree/development_server",
       skills: ["Terraform", "Docker", "AWS", "Packer", "Linux", "CI/CD"],
-      image: "/public/visitor-log-system.gif",
+      image: "/visitor-log-system.gif",
     }
   ],
   experience: [
